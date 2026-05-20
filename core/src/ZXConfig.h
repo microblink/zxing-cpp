f97679f8bbf43a5e4a85936661f80e1dfecce912
Igor Smolkovič <igor.smolkovic@microblink.com>
2026-05-20 15:49:48 +0200

    Add MB patch for `thread_local`

--- core/src/ZXConfig.h
+++ core/src/ZXConfig.h
@@ -10,7 +10,15 @@
 // your platform doesn't support thread_local, you can switch to static, but be aware that this makes the code not thread safe.
 // For Windows in Visual Studio 2019 on Intel 64-bit using thread_local causes a dependency to VCRUNTIME140_1.dll, so you need 2019
 // runtime DLLs instead of only 2015 version.
-#define ZX_THREAD_LOCAL thread_local // '' (nothing), 'thread_local' or 'static'
+// #define ZX_THREAD_LOCAL thread_local // '' (nothing), 'thread_local' or 'static'
+
+// MB patch begin
+#if defined( __EMSCRIPTEN__ )
+#   define ZX_THREAD_LOCAL static
+#else
+#   define ZX_THREAD_LOCAL thread_local
+#endif
+// MB patch end
 
 // The Galois Field abstractions used in Reed-Solomon error correction code use more memory than required to improve performance (20% - 100%).
 // If RAM is scarce, you can define LIBRSCPP_SAVE_MEMORY to reduce memory usage. The effect is a few kB big.
